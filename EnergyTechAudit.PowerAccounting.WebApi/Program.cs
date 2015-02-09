d68bf49d711a012fc5079e4f62bc3bc7abdbba1b
eta-development-team <eta.development.team@outlook.com>
2015-02-09 10:23:05 +0300

    111

---
+++ EnergyTechAudit.PowerAccounting.WebApi/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnergyTechAudit.PowerAccounting.WebApi
+{
+    internal class Program
+    {
+
+        public static void Main()
+        {
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                var startDateTime = DateTime.Parse("2015-01-01");
+                var endDateTime = DateTime.Parse("2015-01-31");
+
+                // для возможности использования самоподписанного сертификата сервера 
+                // после установки на сервер заверенного сертификата вызов данного обработчика будет не нужен 
+                ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) =>
+                {
+                    return true;
+                };
+                client.BaseAddress = new Uri("https://192.168.1.2"); // https://eta.asd116.ru
+
+                client.DefaultRequestHeaders.Add("login", "Archive.Downloader");
+                client.DefaultRequestHeaders.Add("password", "xxxxxx");
+
+                var requestUri = string.Format
+                (
+                    "/api/package/archive?" +
+                    "ar.withDictionaries=true&" +
+                    "ar.startDateTime={0:yyyy-MM-dd}&" +
+                    "ar.endDateTime={1:yyyy-MM-dd}&" +
+                    "responseToFile=true",
+
+                    startDateTime,
+                    endDateTime
+                );
+
+                var response = client
+                    .GetAsync(requestUri)
+                    .Result;
+
+                if (response != null && response.StatusCode == HttpStatusCode.OK)
+                {
+                    byte[] content = response.Content.ReadAsByteArrayAsync().Result;
+
+                    string path = @"d:\ArchivePackageCs.xml";
+                    if (File.Exists(path))
+                    {
+                        File.Delete(path);
+                    }
+                    File.WriteAllBytes(path, content);
+
+                }
+            }
+        }
+    }
+}
